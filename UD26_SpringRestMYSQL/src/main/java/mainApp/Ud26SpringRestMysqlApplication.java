b79e496730d2366344313e81b8c826392cd4051d
Skice7 <55830756+Skice7@users.noreply.github.com>
2022-03-04 20:31:50 +0100

    commit Ejercicio4

---
+++ UD26_SpringRestMYSQL/src/main/java/mainApp/Ud26SpringRestMysqlApplication.java
@@ -0,0 +1,13 @@
+package mainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ud26SpringRestMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ud26SpringRestMysqlApplication.class, args);
+	}
+
+}

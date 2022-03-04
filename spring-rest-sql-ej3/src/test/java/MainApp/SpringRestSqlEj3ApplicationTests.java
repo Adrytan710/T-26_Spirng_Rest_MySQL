d1a8386522259477a1f4b0668b612e6be560c056
Samuel Zecirovic <55589454+SamuelZecirovic@users.noreply.github.com>
2022-03-04 18:44:58 +0100

    commit act3 esta vez si

---
+++ spring-rest-sql-ej3/src/test/java/MainApp/SpringRestSqlEj3ApplicationTests.java
@@ -0,0 +1,13 @@
+package MainApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSqlEj3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d39850c496f04d1a14b9c2c8923c6f9dfd97af9e
Samuel Zecirovic <55589454+SamuelZecirovic@users.noreply.github.com>
2022-03-04 19:23:57 +0100

    commmmmmmmit

---
+++ spring-rest-sql-ej3/src/main/java/MainApp/SpringRestSqlEj3Application.java
@@ -0,0 +1,13 @@
+package MainApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSqlEj3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSqlEj3Application.class, args);
+	}
+
+}
